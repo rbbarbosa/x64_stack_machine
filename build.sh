542c3aa6b8d36ffdf2faf80980ac39ba9f0c2e0c
Raul Barbosa <rbarbosa@dei.uc.pt>
2021-11-17 14:48:03 +0000

    simpler names for files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-yacc -y -d -t -v postfix64.y                         # generate y.tab.c, y.tab.h
-cc y.tab.c -Wall -Wno-unused-function -o postfix64   # compile and link
+yacc -y -d -t -v stackmachine.y                         # generate y.tab.c, y.tab.h
+cc y.tab.c -Wall -Wno-unused-function -o stackmachine   # compile and link
--- examples/build.sh
+++ examples/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-../postfix64/postfix64 < simple.in > simple.s     # generate x86-64 code for expression in simple.in
-cc simple.s simple.c -o simple                    # compile and link the .s and .c files
+../stackmachine < simple.in > simple.s     # generate x86-64 code for expression in simple.in
+cc simple.s simple.c -o simple             # compile and link the .s and .c files
 
-../postfix64/postfix64 < compare.in > compare.s   # generate x86-64 code for expression in simple.in
-cc -O0 compare.s compare.c -o compare             # compile and link the .s and .c files
+../stackmachine < compare.in > compare.s   # generate x86-64 code for expression in simple.in
+cc -O0 compare.s compare.c -o compare      # compile and link the .s and .c files
